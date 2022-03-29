4d453cb66c627b31cf07969c1eda064b4eb7d9a8
Mubashir78 <mubashirrindbaloch@gmail.com>
2022-03-29 18:44:57 +0500

    v1.0.1
    . Decreased the time difference between printing texts
    . Made the code more readable
    . Added an .exe version to be able to run without the need of Python and VSC

--- Q-A_Game.py
+++ Q-A_Game.py
@@ -8,7 +8,7 @@ def print_slow(str):
     for letter in str:
         stdout.write(letter)
         stdout.flush()
-        sleep(0.03)
+        sleep(0.025)
     print(" ")
 
 def question_maker(Question, cor_ans):
@@ -17,6 +17,7 @@ def question_maker(Question, cor_ans):
     sleep(1)
     print_slow(f"{Question}")
     sleep(1.2)
+
     while True:
         user_ans = input("\nYour answer: ").lower()
         if user_ans in cor_ans:
@@ -25,6 +26,7 @@ def question_maker(Question, cor_ans):
             print_slow("Let's move on to the next question.")
             sleep(1)
             break
+
         else:
             print_slow("Wrong answer, please try again.")
             sleep(1.2)
@@ -32,12 +34,17 @@ def question_maker(Question, cor_ans):
 def no_more_questions():
     print_slow("\nOops, looks like that's all for now.")
     sleep(1)
-    print_slow(f"You answered {Q.Q_count-1} questions so far.")
+
+    print_slow(f"You answered {Q.Q_count} questions so far.")
     sleep(1.2)
+
     print_slow("\nThank you for playing!")
     sleep(0.6)
+
     print_slow("\nThe script will now exit.")
     sleep(1)
+
+    return
 
 print_slow("Welcome to a game under progress.")
 sleep(1)
@@ -45,8 +52,12 @@ ready = input("Are you ready to play?(y/n): ").lower()
 
 if ready == "y":
     question_maker("How many colours are there in the rainbow?", ["7", "seven"])
+
     question_maker("What is my name?", ["mubashir", "ahmed", "mubashir ahmed"])
+
     question_maker("How many days are there in a year?", ["365", "three sixty five", "three-sixty-five"])
+
     question_maker("What is the name of the highest peak/mountain?",["everest", "mount everest"])
-    question_maker("Who is called the father of computers?", ["charles babbage"])
+
+    question_maker("Who is called the father of computers?", ["babbage", "charles babbage"])
     no_more_questions()
